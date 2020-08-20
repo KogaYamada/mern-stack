b20e529ca3d187b95f0a6aa480cf01ab9d21fe41
KogaYamada <koga0087y@gmail.com>
2020-08-20 21:34:45 +0900

    add client

--- server/resolvers/post.js
+++ server/resolvers/post.js
@@ -1,9 +1,32 @@
 const { gql } = require('apollo-server-express');
 
-const totalPosts = () => 42;
+const { posts } = require('../temp');
+
+// queries
+const totalPosts = () => posts.length;
+const allPosts = () => posts;
+
+// mutations
+const newPost = (parrent, args, context) => {
+  console.log(args);
+  // create a new post object
+  const post = {
+    id: posts.length + 1,
+    ...args.input,
+  };
+
+  // push new post object to posts array
+  posts.push(post);
+
+  return post;
+};
 
 module.exports = {
   Query: {
     totalPosts,
+    allPosts,
+  },
+  Mutation: {
+    newPost,
   },
 };
