de9bdb80a1057a2351d03682adb0c1902cf7a88b
KogaYamada <koga0087y@gmail.com>
2020-08-21 20:37:02 +0900

    ログイン実装

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,7 +1,22 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { auth } from '../firebase';
+import { AuthContext } from '../context/authContext';
 
 const Nav = () => {
+  const { state, dispatch } = useContext(AuthContext);
+  let history = useHistory();
+
+  const { user } = state;
+
+  const logout = () => {
+    auth.signOut();
+    dispatch({
+      type: 'LOGGED_IN_USER',
+      payload: null,
+    });
+    history.push('/login');
+  };
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <Link className="navbar-brand" to="/">
@@ -20,16 +35,27 @@ const Nav = () => {
 
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav mr-auto">
-          <li className="nav-item active">
-            <Link className="nav-link" to="/login">
-              Login
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/register">
-              Register
-            </Link>
-          </li>
+          {!user && (
+            <>
+              <li className="nav-item active">
+                <Link className="nav-link" to="/login">
+                  Login
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/register">
+                  Register
+                </Link>
+              </li>
+            </>
+          )}
+          {user && (
+            <li className="nav-item">
+              <a onClick={logout} href="/login" className="nav-item nav-link">
+                Logout
+              </a>
+            </li>
+          )}
         </ul>
         <form className="form-inline my-2 my-lg-0">
           <input
--- client/src/pages/auth/Login.js
+++ client/src/pages/auth/Login.js
@@ -1,15 +1,70 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { AuthContext } from '../../context/authContext';
+import { auth, googleAuthProvider } from '../../firebase';
 
 const Login = () => {
+  const { dispatch } = useContext(AuthContext);
   const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const [loading, setLoading] = useState(false);
+  const [success, setSuccess] = useState(false);
 
-  const handleSubmit = (e) => {
+  let history = useHistory();
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
+    try {
+      await auth
+        .signInWithEmailAndPassword(email, password)
+        .then(async (result) => {
+          const { user } = result;
+          const idTokenResult = await user.getIdTokenResult();
+
+          dispatch({
+            type: 'LOGGED_IN_USER',
+            payload: {
+              email: user.email,
+              token: idTokenResult.token,
+            },
+          });
+
+          // send user info to our server mongodb to either update/create
+
+          history.push('/');
+        });
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+      setLoading(false);
+    }
+  };
+
+  const googleLogin = () => {
+    auth.signInWithPopup(googleAuthProvider).then(async (result) => {
+      const { user } = result;
+      const idTokenResult = await user.getIdTokenResult();
+
+      dispatch({
+        type: 'LOGGED_IN_USER',
+        payload: {
+          email: user.email,
+          token: idTokenResult.token,
+        },
+      });
+
+      // send user info to our server mongodb to either update/create
+
+      history.push('/');
+    });
   };
   return (
     <div className="container p-5">
-      <h4>Login</h4>
+      {loading ? <h4 className="text-danger">Loading...</h4> : <h4>Login</h4>}
+      <button onClick={googleLogin} className="btn btn-raised btn-danger mt-5">
+        Login With Google
+      </button>
       <form onSubmit={handleSubmit}>
         <div className="form-group">
           <label>Email address</label>
@@ -22,12 +77,23 @@ const Login = () => {
             disabled={loading}
           />
         </div>
+        <div className="form-group">
+          <label>Password</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="form-control"
+            placeholder="Enter Password"
+            disabled={loading}
+          />
+        </div>
+        <button
+          className="btn btn-raised btn-primary"
+          disabled={!email || !password || loading}>
+          Submit
+        </button>
       </form>
-      <button
-        className="btn btn-raised btn-primary"
-        disabled={!email || loading}>
-        Submit
-      </button>
     </div>
   );
 };
