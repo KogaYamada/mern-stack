5497105345b6b1336e31369de8588e16d0fa6869
KogaYamada <koga0087y@gmail.com>
2020-08-23 10:57:55 +0900

    useMemo

--- client/src/pages/auth/Profile.js
+++ client/src/pages/auth/Profile.js
@@ -1,7 +1,51 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
+import { toast } from 'react-toastify';
+import { useQuery, useMutation, gql } from '@apollo/client';
+
+const PROFILE = gql`
+  query {
+    profile {
+      _id
+      name
+      username
+      email
+      images {
+        url
+        public_id
+      }
+      about
+      createdAt
+      updatedAt
+    }
+  }
+`;
 
 const Profile = () => {
-  return <h4>profile</h4>;
+  const [values, setValues] = useState({
+    username: '',
+    email: '',
+    name: '',
+    about: '',
+    images: [],
+  });
+  const [loading, setLoading] = useState(false);
+
+  const { data } = useQuery(PROFILE);
+
+  useMemo(() => {
+    if (data) {
+      console.log(data);
+      setValues({
+        username: data.profile.username,
+        email: data.profile.email,
+        name: data.profile.name,
+        about: data.profile.about,
+        images: data.profile.images,
+      });
+    }
+  }, []);
+
+  return <div className="container p-5">{JSON.stringify(values)}</div>;
 };
 
 export default Profile;
